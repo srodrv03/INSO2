dc72a96a15f9474eddc4429d7b136c30abdfea2e
srodrv03 <srodrv03@estudiantes.unileon.es>
2021-04-09 15:29:11 +0200

    Pequeños cambios en el backend

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var app = express();
-
-
-
-
 const db = require("./database/db")
 
 db.sequelize.authenticate().then(() => {
@@ -13,5 +9,5 @@ db.sequelize.authenticate().then(() => {
 });
 
 app.listen(3000, function () {
-    console.log('¡Aplicación escuchando en el puerto 3000!');
+  console.log('¡Aplicación escuchando en el puerto 3000!');
 });
