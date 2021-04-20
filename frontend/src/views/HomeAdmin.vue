378f447121ba41ecd16d85f63fbf96b88a725563
srodrv03 <srodrv03@estudiantes.unileon.es>
2021-04-20 22:05:26 +0200

    Creaccion de la tabla y el selector

--- frontend/src/views/HomeAdmin.vue
+++ frontend/src/views/HomeAdmin.vue
@@ -1,9 +1,61 @@
 <template>
   <v-main>
-    <v-row class="text-center">
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Bienvenido sergfswgf</h1>
+    <v-row class="text-center"  justify=center>
+      <v-col class="mb-4" cols=10 >
+        <v-card>
+          <v-tabs background-color="primary" center-active dark>
+            <v-tab @click="obtenerEmpleados()">Empleados</v-tab>
+            <v-tab @click="obtenerClientes()">Clientes</v-tab>
+            <v-tab @click="obtenerVehiculos()">Vehiculos</v-tab>
+          </v-tabs>
+          <v-data-table
+            dense
+            :headers="headers"
+            :items="desserts"
+            item-key="name"
+            class="elevation-1"
+          >
+          <template v-slot:item.venderbut="{ item }">
+              <v-btn
+                color="green lighten-1"
+                @click="creaDialogVenta(item)"
+                small
+                v-model="item.venderbut"
+              >Vender</v-btn>
+            </template>
+            <template v-slot:item.comprarbut="{ item }">
+              <v-btn
+                @click="creaDialogCompra(item)"
+                color="red"
+                small
+                v-model="item.comprarbut"
+              >Comprar</v-btn>
+            </template></v-data-table>
+        </v-card>
       </v-col>
     </v-row>
-    </v-main>
+  </v-main>
 </template>
+<script>
+export default {
+  data: () => ({
+    desserts: [
+      {
+        simbolo:"prueba",
+        precio:"32"
+      }
+    ],
+     headers: [
+      {
+        text: "Simbolo",
+        align: "start",
+        sortable: true,
+        value: "simbolo"
+      },
+      { text: "Valor", value: "precio" },
+      { text: "", value: "venderbut" },
+      { text: "", value: "comprarbut" }
+    ],
+  }),
+};
+</script>
