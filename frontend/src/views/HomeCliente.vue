ea282c0d94e10004a7cdf18336a14b900263216c
srodrv03 <srodrv03@estudiantes.unileon.es>
2021-05-10 19:01:17 +0200

    Modificados los componentes

--- frontend/src/views/HomeCliente.vue
+++ frontend/src/views/HomeCliente.vue
@@ -1,18 +1,80 @@
 <template>
-
   <v-app>
-    <Navbar></Navbar>
+    <v-navigation-drawer
+      flat
+      v-model="drawer"
+      app
+      :mini-variant.sync="mini"
+      permanent
+    >
+      <v-card class="mx-auto" width="5000">
+        <v-list-item class="px-2">
+          <v-list-item-avatar>
+            <v-img src="https://randomuser.me/api/portraits/men/85.jpg"></v-img>
+          </v-list-item-avatar>
 
+          <v-list-item-title>John Leider</v-list-item-title>
+
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list dense nav>
+          <v-list-item
+            @click="acciones(item)"
+            v-for="item in items"
+            :key="item.title"
+            link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-navigation-drawer>
+    <v-main fluid>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar";
 export default {
-  name: "App",
-  components: { Navbar },
   data() {
-    return {};
+    return {
+      drawer: true,
+      mini: false,
+      items: [
+        {
+          icon: "mdi-car",
+          title: "Mis vehículos",
+        },
+        {
+          icon: "mdi-tools",
+          title: "Reparaciones",
+        },
+        { icon: "mdi-account", title: "Cuenta" },
+      ],
+    };
   },
+  methods: {
+    acciones(item) {
+      if (item.title == "Mis vehículos") {
+        this.$router.push("/HomeCliente/Vehiculos");
+      } else if (item.title == "Reparaciones") {
+        this.$router.push("/HomeCliente/Reparaciones");
+      } else if (item.title == "Cuenta") {
+        this.$router.push("/HomeCliente/cuenta");
+      }
+    },
+  }
 };
 </script>
